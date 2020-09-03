51500b7d47184c23e96c959dfcd1a3ec9fd98029
nithyamn <nithya.mn999@gmail.com>
2020-09-03 17:03:30 +0530

    Create google.js

---
+++ test/specs/google.js
@@ -0,0 +1,7 @@
+describe('Google Search', () => {
+ it('Google Search - BrowserStack', () => {
+      browser.url('https://www.google.com/');
+      $('[name="q"]').setValue('BrowserStack');
+      browser.getTitle().should.match("Google");
+  });
+})
