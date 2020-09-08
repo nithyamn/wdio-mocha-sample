7a5a5f352bc50fae2a2e780c45a1bfa4d6b8a7fa
nithyamn <nithya.mn999@gmail.com>
2020-09-08 18:27:34 +0530

    Update browserstack_single.conf.js

--- conf/browserstack_single.conf.js
+++ conf/browserstack_single.conf.js
@@ -65,7 +65,7 @@ exports.config = {
     // },
     reporters: [
         [
-          'browserstack',
+          'junit',
           {
             outputDir: './tests/reports/browserstack',
           }
